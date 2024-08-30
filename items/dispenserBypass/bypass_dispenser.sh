6a6a7954f9dc803bbe964ee3414c4cecc019960b
jimmy butzen <jimmybutzen@icloud.com>
2024-08-30 13:58:13 +0200

    fixing typos

--- items/dispenserBypass/bypass_dispenser.sh
+++ items/dispenserBypass/bypass_dispenser.sh
@@ -10,7 +10,7 @@ if [ -z "$containerHash" ]; then
 fi
 
 # Execute the SQL INSERT query inside the Docker container
-docker exec -i "$containerHash" psql -U dual -d dual -c "INSERT INTO element_property (name, property_type, value, element_id, internal) SELECT 'bypassPrimaryContainer', 1, '1', id, false FROM element WHERE element_type_id = 1947803569 ON CONFLICT (element_id, name) DO UPDATE SET property_type = 1 value = '1';"
+docker exec -i "$containerHash" psql -U dual -d dual -c "INSERT INTO element_property (name, property_type, value, element_id, internal) SELECT 'bypassPrimaryContainer', 1, '1', id, false FROM element WHERE element_type_id = 1947803569 ON CONFLICT (element_id, name) DO UPDATE SET property_type = 1, value = '1';"
 
 # Check if the command was successful
 if [ $? -ne 0 ]; then
